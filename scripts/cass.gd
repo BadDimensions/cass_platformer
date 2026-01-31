512f5da4ff50423f402ad3078423808f7f07cd9a
BadDimensions <victoryincolour@gmail.com>
2026-01-31 16:41:28 -0500

    ugh

--- scripts/cass.gd
+++ scripts/cass.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
+enum STATE { MOVE, JUMP, WALLJUMP }
+
+@export var state: = STATE.MOVE
+
 @onready var sprite_2d: Sprite2D = $anchor/Sprite2D
 @onready var anchor: Node2D = $anchor
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+var coyote_time = 0
+
 
 @export var max_speed = 50
 @export var acceleration = 100
@@ -11,35 +19,69 @@ extends CharacterBody2D
 @export var air_friction = 500
 @export var up_gravity = 500
 @export var down_gravity = 600
-@export var jump_amount = 500
+@export var jump_amount = 700
 
-
+func _ready() -> void:
+	animation_player.current_animation_changed.connect(func(animation_name: String):
+		animation_player.play(animation_name)	
+)
 func _physics_process(delta:float) -> void:
-	var x_input = Input.get_axis("move_left", "move_right")
-	velocity.x = x_input * 50
+	match state:
+		STATE.MOVE:
+			coyote_time -= delta
+			
+			var x_input = Input.get_axis("move_left", "move_right")
+			
+			
+			apply_gravity(delta)	
+			
+			if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_time >= 0):
+				animation_player.play("cass_jump")
+				jump()
+			
+			
+			
+			if x_input == 0:
+				apply_friction(delta)
+				animation_player.play("cass_idle")
+			else:
+				accelerate_horizontally(x_input, delta)
+				anchor.scale.x = sign(x_input)
+				animation_player.play("cass_run")
 	
-	if not is_on_floor():
-		velocity.y += up_gravity * delta
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		animation_player.play("cass_jump")
-		velocity.y = -jump_amount
-	
-	apply_gravity(delta)
-	
-	if x_input == 0:
-		apply_friction(delta)
-		animation_player.play("cass_idle")
-	else:
-		accelerate_horizontally(x_input, delta)
-		anchor.scale.x = sign(x_input)
-		animation_player.play("cass_run")
-	
-	if not is_on_floor():
-		animation_player.play("cass_jump")
-		
-	move_and_slide()
-	
+			
+			if not is_on_floor() and velocity.y <= 0:
+				animation_player.play("cass_jump")
+			if not is_on_floor() and velocity.y >= 0:
+				animation_player.play("cass_fall")
+			
+			
+			if Input.is_action_pressed("attack") and not is_on_floor():
+				animation_player.play("cass_jumpattack")
+				
+				
+			if Input.is_action_just_pressed("attack")and is_on_floor():
+				animation_player.play("cass_attack")
+			
+			
+			var was_on_floor: = is_on_floor()
+			move_and_slide()
+			if was_on_floor and not is_on_floor() and velocity.y >= 0:
+				coyote_time = 0.1
+			
+			
+			#if should_wall_jump():
+				#state = STATE.WALLJUMP
+			
+			
+		STATE.JUMP:
+			pass
+		STATE.WALLJUMP:
+			pass
+
+
+func jump() -> void:
+	velocity.y = -jump_amount
 
 func accelerate_horizontally(horizontal_direction: float, delta: float) -> void:
 	var acceleration_amount = acceleration
@@ -57,3 +99,6 @@ func apply_gravity(delta) -> void:
 			velocity.y += up_gravity * delta
 		else:
 			velocity.y += down_gravity * delta
+
+#func should_wall_jump() -> bool:
+	#return is_on_wall_only()
