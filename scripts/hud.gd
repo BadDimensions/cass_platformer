4377e00cde5778dc793aedad0598d940cc5a3bc7
BadDimensions <victoryincolour@gmail.com>
2026-02-06 18:42:42 -0500

    werwer

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 signal start_game
+signal game_over
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 func init():
@@ -8,20 +9,18 @@ func init():
 	$Sprite2D.show()
 	$StartButton.show()
 
-func show_message(text): 
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
+#func show_message(text): 
+	#$Message.text = text
+	#$Message.show()
+	#$MessageTimer.start()
 	
 func show_game_over():
-	show_message("Game Over")
-	await $MessageTimer.timeout
 	get_tree().paused = true
-	show()	
-	
+	$GameOver.show()
+	await $MessageTimer.timeout
 	$Sprite2D.show()
-	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
+
 func _on_game_start():
 	get_tree().paused = false
 	show()	
@@ -36,4 +35,4 @@ func _on_start_button_pressed():
 	#cass can still move while the menu is up you dont have to start the game to
 	#start the game?
 func _on_message_timer_timeout():
-		$Message.hide()
+		$GameOver.hide()
