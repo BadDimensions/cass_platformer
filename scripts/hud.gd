52c9aa4ae264217e0f1fbfdc2e2a0deef2829797
Stuart <11183054+stuartwk@users.noreply.github.com>
2026-02-07 21:39:46 +0900

    fixes start game, game over, and game reset flow

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,22 +5,28 @@ signal game_over
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-func init():
+func _ready():
 	get_tree().paused = true
 	$Sprite2D.show()
 	$StartButton.show()
 	$GameOver.hide()
-	
+
 func show_game_over():
+	# _on_start_button_pressed we hide the entire hud canvase
+	# we need to show it again
+	show();
+	
 	get_tree().paused = true
 	$GameOver.show()
-	await $MessageTimer.timeout
+	$MessageTimer.start() # have to start the timer
+	await $MessageTimer.timeout # then wait for the timeout
 	$Sprite2D.show()
+	$GameOver.hide()
 	$StartButton.show()
 
 func _on_game_start():
 	get_tree().paused = false
-	$Sprite2D.show()	
+	$Sprite2D.show()
 	$GameOver.hide()
 func _on_start_button_pressed():
 	$Sprite2D.hide()
@@ -28,8 +34,6 @@ func _on_start_button_pressed():
 	start_game.emit()
 	get_tree().paused = false
 	hide()
-	
-	#cass can still move while the menu is up you dont have to start the game to
-	#start the game?
+
 func _on_message_timer_timeout():
 		$GameOver.hide()
